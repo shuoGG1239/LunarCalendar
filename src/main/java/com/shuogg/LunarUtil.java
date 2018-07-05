d112e3cf50aec39f291c455c3ad9aa9a0ac4d311
shuoGG <328893769@qq.com>
2018-07-05 11:15:34 +0800

    add doc

--- src/main/java/com/shuogg/LunarUtil.java
+++ src/main/java/com/shuogg/LunarUtil.java
@@ -9,10 +9,26 @@ public class LunarUtil {
     private LunarUtil() {
     }
 
+    /**
+     * 农历转公历
+     *
+     * @param year
+     * @param month
+     * @param day
+     * @return
+     */
     public static LocalDate lunar2Solar(int year, int month, int day) {
         return LunarCalendar.lunar2Solar(year, month, day);
     }
 
+    /**
+     * 公历转农历
+     *
+     * @param year
+     * @param month
+     * @param day
+     * @return
+     */
     public static LunarCalendar solar2Lunar(int year, int month, int day) {
         return LunarCalendar.solar2Lunar(year, month, day);
     }
